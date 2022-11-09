1f77ca322648866f0d8d0d30b74fd2735a24a0d6
itsdivgithub <divsng.786@gmail.com>
2022-11-10 02:27:20 +0530

    1

---
+++ gallery/item-list/ItemList.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import './ItemList.css'
+import {
+  Typography,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+} from '@material-ui/core'
+import imageHolder from '../../../../images/imageHolder.jpg'
+import CircularStatic from '../../../commons/CircularProgressWithLabel'
+
+function ItemList({ account, contract, setSelectedProfile }) {
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+  const [data, setData] = useState([])
+
+  const getImage = (ipfsURL) => {
+    if (!ipfsURL) return
+    ipfsURL = ipfsURL.split('://')
+    return 'https://ipfs.io/ipfs/' + ipfsURL[1]
+  }
+
+  useEffect(() => {
+    const tempItems = []
+    const loadsData = async () => {
+      try {
+        setLoading(true)
+
+        // returns a number of  all ids from smaart contract
+        const itemsLengthSC = await contract._ids()
+        for (let i = 2; i <= itemsLengthSC; i++) {
+          // thelist is list of campus from SC
+          let item = await contract.thelist(i)
+          // creating new obj, will hold all data & push it to tempItems
+          let curObj = {
+            contractData: item,
+          }
+
+          //  use to get metadata from IPFS
+          const cid = item.cid
+
+          //  get data from IPFS using cid
+          let res = await fetch(cid)
+          res = await res.json()
+          // converts string to obj
+          let description = JSON.parse(res.description)
+          // assembling curObj
+          curObj.description = description.description
+          curObj.category = description.category
+          curObj.price = description.price
+          curObj.condition = description.condition
+          curObj.retailPrice = description.retailPrice
+          curObj.location = description.location
+          curObj.walletAddress = description.walletAddress
+          curObj.created = description.created
+          curObj.image = getImage(res.image)
+          curObj.name = res.name
+          tempItems.push(curObj)
+        }
+        setData(tempItems)
+        setLoading(false)
+      } catch (error) {
+        console.log(error)
+        setLoading(false)
+      }
+    }
+    loadsData()
+  }, [contract])
+
+  const details = (profile) => {
+    localStorage.removeItem('selectedItem')
+    localStorage.setItem('selectedItem', profile)
+    setSelectedProfile(profile)
+    history.push(`/details/${profile.cid}`)
+  }
+
+  return (
+    <div style={{ minHeight: '60vh', borderRadius: '24px' }}>
+      {loading ? (
+        <CircularStatic />
+      ) : (
+        <div>
+          <Grid container spacing={40}>
+            {data.length ? (
+              data.map((profile, index) => (
+                <Grid item md={3} spacing={1} className="swap-card">
+                  <Card sx={{ maxWidth: 300 }} onClick={() => details(profile)}>
+                    <CardMedia
+                      component="img"
+                      height="300"
+                      image={profile.image || imageHolder}
+                      alt="Profile"
+                    />
+                    <CardContent>
+                      <Typography className="card-price">
+                        ${profile.price ? profile.price : 'FREE'}
+                      </Typography>
+                      <Typography
+                        color="#1C1A19"
+                        className="card-header-swap spacer"
+                      >
+                        {profile.name || profile.username}
+                      </Typography>
+                      <Typography className="card-location spacer">
+                        {profile.location ? profile.location : 'New York City'}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+            ) : (
+              <h2>No Items Yet...</h2>
+            )}
+          </Grid>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ItemList
---
+++ item-details/ItemDetails.js
@@ -0,0 +1,165 @@
+import React, { useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import FavoriteIcon from '@material-ui/icons/Favorite'
+import ShareIcon from '@material-ui/icons/Share'
+import Card from '@material-ui/core/Card'
+import { Link } from 'react-router-dom'
+import { StylesProvider } from '@material-ui/core/styles'
+import { IconButton } from '@material-ui/core'
+import './ItemDetails.css'
+import detailsImg from '../../../images/details.png'
+import imageHolder from '../../../images/imageHolder.jpg'
+import { CircularStatic } from '../../commons/CircularProgressWithLabel'
+
+function ItemDetails({ account, contract, selectedItem }) {
+  console.log('SelectedItem', selectedItem)
+  console.log(
+    'selectedItem.contractData.isSold',
+    selectedItem.contractData.isSold,
+  )
+  const [comment, setComment] = useState('')
+  const [codeHash, setCodeHash] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = (event) => {
+    setComment(event.target.value)
+  }
+
+  const mintNFT = async () => {
+    console.log(
+      'contractData.id._hex == ',
+      selectedItem.contractData.id.toString(),
+    )
+    try {
+      const curItemID = selectedItem.contractData.id.toString()
+      const res = await contract.buyCampus(curItemID)
+      console.log('MINT res', res)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <StylesProvider injectFirst>
+      <Container className="root-pet-details">
+        <div className="">
+          <Grid container spacing={6}>
+            <Grid item xs={12} sm={6} className="grid-container">
+              {selectedItem.contractData.isSold ? (
+                <Button
+                  variant="contained"
+                  className="wallet-btn"
+                  color="primary"
+                >
+                  Sold
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  className="wallet-btn"
+                  color="primary"
+                  onClick={mintNFT}
+                >
+                  Buy now
+                </Button>
+              )}
+
+              <br />
+              <div style={{ textAling: 'center' }}></div>
+
+              <center>
+                <img
+                  className="img"
+                  src={selectedItem.image ? selectedItem.image : imageHolder}
+                  alt="pet"
+                />
+                <div className="flex-container">
+                  <div>
+                    <IconButton aria-label="add to favorites">
+                      <FavoriteIcon />
+                    </IconButton>
+                  </div>
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+                </div>
+              </center>
+
+              <div className="specifications">
+                <h2 style={{ margin: '3px' }}>Description</h2>
+                <Typography
+                  gutterBottom
+                  variant="subtitle1"
+                  color="textPrimary"
+                >
+                  {selectedItem.description}
+                </Typography>
+              </div>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Card className="code-hash">
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  className="ingredients-title "
+                >
+                  {selectedItem.name}
+                </Typography>
+
+                <p className="price">
+                  ${selectedItem.price ? selectedItem.price : 0}
+                </p>
+                <p className="retailPrice price">
+                  Retail Price: ${selectedItem.retailPrice}
+                </p>
+                <p className="category price">
+                  {' '}
+                  Category: {selectedItem.category}
+                </p>
+                <p className="condition price">
+                  Condition: {selectedItem.condition}
+                </p>
+
+                <p className="location price">
+                  Location: {selectedItem.location}
+                </p>
+                <img
+                  src={detailsImg}
+                  alt="detailsImg"
+                  className="direction-img"
+                />
+                <br />
+                <div className="chef-profile">
+                  <Link>
+                    <Typography>
+                      Listed by <strong>{selectedItem.walletAddress}</strong>
+                    </Typography>
+                  </Link>
+                </div>
+              </Card>
+
+              <div className="code-hash">
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  className="ingredients-title"
+                >
+                  <p className="created ">Created: {selectedItem.created}</p>
+                  Directions
+                </Typography>
+                There are no directions for this item.it is recommendable to
+                meet in a public place.
+              </div>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </StylesProvider>
+  )
+}
+
+export default ItemDetails
